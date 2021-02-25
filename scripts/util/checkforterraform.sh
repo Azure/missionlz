42a16eff359d5a3e79a8200507792fd0b4560ebb
Brooke Hamilton <brooke.hamilton@microsoft.com>
2021-02-25 15:13:40 -0500

    initial commit

---
+++ scripts/util/checkforterraform.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Check for Terraform
+if ! command -v terraform &> /dev/null; then
+    echo "terraform could not be found. This script requires the Terraform CLI."
+    echo "see https://learn.hashicorp.com/tutorials/terraform/install-cli for installation instructions."
+    exit 1
+fi
---
+++ src/modules/networkwatcher/variables.tf
@@ -0,0 +1,15 @@
+variable name_prefix {
+  description = "A prefix for naming network watcher resources"
+  type        = string
+}
+
+variable location {
+  description = "Location for network watcher resources (only one Azure Network Watcher per-sub-per-region)"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
