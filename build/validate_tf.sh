4059abbdccfaf28f9f20e4fab0f41b2c4c0984a5
Glenn Musa <glennmusa@users.noreply.github.com>
2021-02-25 16:44:01 -0500

    validate terraform in src/core

--- build/validate_tf.sh
+++ build/validate_tf.sh
@@ -20,32 +20,35 @@ if ! command -v terraform &> /dev/null; then
     exit 1
 fi
 
-# Navigate to the root of the repository
-cd .. || exit
-rootdir=$(pwd)
+full_path=$(realpath "${0}")
+repo_path=$(dirname "$(dirname "${full_path}")")
+core_path="${repo_path}/src/core"
 
 # Validate all .tf files
-program_log "Validating all terraform"
+program_log "Validating Terraform..."
+cd "${core_path}" || exit
 for i in $(find . -name "*.tf" -printf "%h\n" | sort --unique)
 do
   cd "${i}" || exit
   echo "validating ${i}..."
   terraform init -backend=false >> /dev/null || exit 1
   terraform validate >> /dev/null || exit 1
-  cd "${rootdir}" || exit
+  cd "${core_path}" || exit
 done
-program_log "All terraform validated successfully"
+program_log "Terraform validated successfully!"
 
 # Check formatting for all .tf files
-program_log "Linting all terraform"
+program_log "Linting Terraform..."
+cd "${repo_path}" || exit
 if terraform fmt -check -recursive >> /dev/null;
 then
-  program_log "All terraform linted successfully"
+  program_log "Terraform linted successfully!"
 else
   linting_results=$(terraform fmt -check -recursive)
   for j in $linting_results
   do
     error_log "please format '${j}' with the command 'terraform fmt'"
   done
+  program_log "alternatively, you can run 'terraform fmt -recursive' to format all *.tf in a directory"
   exit 1;
 fi
