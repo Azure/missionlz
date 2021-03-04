22abae296c5b1a2a7f18c1168d6a2920e9692f27
Glenn Musa <glennmusa@users.noreply.github.com>
2021-03-04 17:53:50 -0500

    use vars

--- build/apply_tf.sh
+++ build/apply_tf.sh
@@ -7,18 +7,22 @@ error_log() {
 }
 
 usage() {
-  echo "apply_tf.sh: Automation that calls apply terraform given a MLZ configuration and some globals.tfvars"
-  error_log "usage: apply_tf.sh <path to a mlz config> <path to a globals.tfvars>"
+  echo "apply_tf.sh: Automation that calls apply terraform given a MLZ configuration and some tfvars"
+  error_log "usage: apply_tf.sh <mlz config> <globals.tfvars> <saca.tfvars> <tier0.tfvars> <tier1.tfvars> <tier2.tfvars>"
 }
 
-if [[ "$#" -lt 2 ]]; then
+if [[ "$#" -lt 6 ]]; then
    usage
    exit 1
 fi
 
-# take some valid, well known, mlz_config and global_vars from as input
+# take some valid, well known, mlz_config and vars as input
 mlz_config=$1
 globals=$2
+saca_vars=$3
+tier0_vars=$4
+tier1_vars=$5
+tier2_vars=$6
 
 # reference paths
 core_path=$(realpath ../src/core/)
@@ -31,13 +35,23 @@ scripts_path=$(realpath ../scripts/)
 apply() {
   name=$1
   path=$2
-  . "${scripts_path}/config/generate_vars.sh" "${mlz_config}" "${mlz_config_subid}" "${name}"
-  touch "${path}/dummy.tfvars"
+  vars=$3
+
+  . "${scripts_path}/config/generate_vars.sh" "${mlz_config}" "${mlz_config_subid}" "${name}" "${path}"
+
+  # remove any existing terraform initialzation
+  rm -rf "${path}/.terraform"
+
+  # remove any tfvars and subtitute it
+  tf_vars="${path}/${name}.tfvars"
+  rm -rf "${tf_vars}"
+  cp "${3}" "${tf_vars}"
+
   "${scripts_path}"/apply_terraform.sh "${globals}" "${path}"
 }
 
 # apply terraform
-apply "saca-hub" "${core_path}/saca-hub"
-apply "tier-0" "${core_path}/tier-0"
-apply "tier-1" "${core_path}/tier-1"
-apply "tier-2" "${core_path}/tier-2"
+apply "saca-hub" "${core_path}/saca-hub" "${saca_vars}"
+apply "tier-0" "${core_path}/tier-0" "${tier0_vars}"
+apply "tier-1" "${core_path}/tier-1" "${tier1_vars}"
+apply "tier-2" "${core_path}/tier-2" "${tier2_vars}"
--- build/destroy_tf.sh
+++ build/destroy_tf.sh
@@ -7,18 +7,22 @@ error_log() {
 }
 
 usage() {
-  echo "destroy_tf.sh: Automation that calls destroy terraform given a MLZ configuration and some globals.tfvars"
-  error_log "usage: destroy_tf.sh <path to a mlz config> <path to a globals.tfvars>"
+  echo "destroy_tf.sh: Automation that calls destroy terraform given a MLZ configuration and some tfvars"
+  error_log "usage: destroy_tf.sh <mlz config> <globals.tfvars> <saca.tfvars> <tier0.tfvars> <tier1.tfvars> <tier2.tfvars>"
 }
 
-if [[ "$#" -lt 2 ]]; then
+if [[ "$#" -lt 6 ]]; then
    usage
    exit 1
 fi
 
-# take some valid, well known, mlz_config and global_vars from as input
+# take some valid, well known, mlz_config and vars as input
 mlz_config=$1
 globals=$2
+saca_vars=$3
+tier0_vars=$4
+tier1_vars=$5
+tier2_vars=$6
 
 # reference paths
 core_path=$(realpath ../src/core/)
@@ -31,13 +35,23 @@ scripts_path=$(realpath ../scripts/)
 destroy() {
   name=$1
   path=$2
+  vars=$3
+
   . "${scripts_path}/config/generate_vars.sh" "${mlz_config}" "${mlz_config_subid}" "${name}"
-  touch "${path}/dummy.tfvars"
+
+  # remove any existing terraform initialzation
+  rm -rf "${path}/.terraform"
+
+  # remove any tfvars and subtitute it
+  tf_vars="${path}/${name}.tfvars"
+  rm -rf "${tf_vars}"
+  cp "${3}" "${tf_vars}"
+
   "${scripts_path}"/destroy_terraform.sh "${globals}" "${path}"
 }
 
 # destroy terraform
-destroy "tier-2" "${core_path}/tier-2"
-destroy "tier-1" "${core_path}/tier-1"
-destroy "tier-0" "${core_path}/tier-0"
-destroy "saca-hub" "${core_path}/saca-hub"
+destroy "tier-2" "${core_path}/tier-2" "${tier2_vars}"
+destroy "tier-1" "${core_path}/tier-1" "${tier1_vars}"
+destroy "tier-0" "${core_path}/tier-0" "${tier0_vars}"
+destroy "saca-hub" "${core_path}/saca-hub" "${saca_vars}"
