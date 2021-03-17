23f2923f5588500429c4333fe113f4a047d79782
Bree Stryker <>
2021-03-17 12:39:09 -0400

    Minor changes, and walker.

--- src/front/lib/utils.py
+++ src/front/lib/utils.py
@@ -1,12 +1,49 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # Provides a set of utility functions to be called from the primary API
+import os
+import json
+from dominate.tags import *
 
-# Resolves a dotted property name to a target in a dictionary
-# Will step through and create parent branches if they don't exist
-def dotted_write(prop_name, target_dict):
+def dotted_write(prop_name, val, target_dict):
+    """
+    Purpose:  Function takes in a property value to be mapped that contains .'s in a string.
+    These .'s are resolved to a position in the dictionary in which the value will be written.
+    This function takes advantage of python's pass by value and embeds a value into an originating dictionary
+    that will be re-jsonified and written back to it's location.
+
+    prop_name: a property value from a front end map dict
+    val:  the value to be written tot he target_dict
+    target_dict: the dictionary that the value will be written into
+    """
+
     if prop_name.contains("."):
         pass
 
+def find_config(dir_scan=['core', 'modules']):
+    """
+    Purpose: Function takes a list of directory names.  Performs an os.walk to find *.front.json files and returns a
+    dictionary of file names and their contents.
+
+    dir_scan: the list of directories to be scanned.
+    """
+    config_files = {}
+    for config_dir in dir_scan:
+        walk = os.walk(os.join(os.getcwd(), config_dir))
+        for root, _, files in walk:
+            for f_name in files:
+                if ".front.json" in f_name:
+                    cur_file = os.join(root, name)
+                    config_files["cur_file"] = json.load(cur_file)
+
+    return config_files
+
+
 def dotted_build_form(form_doc):
+    """
+    Purpose:  Function takes in a json document that describes a form, and returns the resulting dominate based
+    form to be appended to the front end UI
+
+    form_doc: a dictionary derived from a loaded json
+    """
     pass
