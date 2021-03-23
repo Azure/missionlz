cb343f64d45f7a5b23684d4ae270da20a9bfb629
Glenn Musa <glennmusa@users.noreply.github.com>
2021-03-23 13:05:30 -0400

    update copyright

--- src/scripts/pre_transfer_setup.sh
+++ src/scripts/pre_transfer_setup.sh
@@ -7,18 +7,7 @@
 # SC1090: Can't follow non-constant source. Use a directive to specify location.
 # SC1091: Not following. Shellcheck can't follow non-constant source.
 #
-# This script deploys container registries, app registrations, and a container instance to run the MLZ front end
-#!/bin/bash
-#
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-#
-# shellcheck disable=SC1090,SC1091
-# SC1090: Can't follow non-constant source. Use a directive to specify location.
-# SC1091: Not following. Shellcheck can't follow non-constant source.
-#
 # This script dumps the docker image and compresses it for transport to a target network
-
 
 # Run Docker Setup Script
 
