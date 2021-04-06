f1ded4ca704300027e63ed524b48eb979cddf718
Byron Boudreaux <16844071+Phydeauxman@users.noreply.github.com>
2021-04-06 14:18:52 -0400

    - Moved loop logic to function

--- src/scripts/config/mlz_config_create.sh
+++ src/scripts/config/mlz_config_create.sh
@@ -20,6 +20,46 @@ usage() {
   error_log "usage: mlz_config_create.sh <mlz config>"
 }
 
+sp_exists () {
+
+    sp_name=$1
+    sp_property=$2
+
+    sp_query="az ad sp show \
+        --id http://${sp_name} \
+        --query sp_property"
+
+    if ! $sp_query &> /dev/null; then
+
+        sleep_time_in_seconds=10
+        max_wait_in_minutes=3
+        max_wait_in_seconds=180
+        max_retries=$((max_wait_in_seconds/sleep_time_in_seconds))
+
+        echo "Maximum time to wait in seconds = ${max_wait_in_seconds}"
+        echo "Maximum number of retries = ${max_retries}"
+        
+        count=1
+
+        while ! $sp_query &> /dev/null
+        do
+
+            echo "Waiting for Service Principal ${sp_property} to complete provisioning (${count}/${max_retries})"
+            echo "Trying again in ${sleep_time_in_seconds} seconds..."
+            sleep "${sleep_time_in_seconds}"
+
+            if [[ ${count} -eq max_retries ]]; then
+                echo "Provisioning the Service Principal ${sp_property} has exceeded ${max_wait_in_minutes} minutes. Investigate and re-run script."
+                exit 1
+            fi
+
+            count=$((count +1))
+
+        done
+    fi
+
+}
+
 if [[ "$#" -lt 1 ]]; then
    usage
    exit 1
@@ -73,39 +113,7 @@ if [[ -z $(az ad sp list --filter "displayName eq '${mlz_sp_name}'" --query "[].
     # Added the sleep below to accomodate for the transient behavior where the Service Principal creation
     # is complete but an immediate query for it will fail. The sleep loop will run for 3 minutes and then
     # the script will exit due to a platform problem
-
-    sp_exists="az ad sp show \
-        --id http://${mlz_sp_name} \
-        --query appId"
-
-    if ! $sp_exists &> /dev/null; then
-
-        sleep_time_in_seconds=10
-        max_wait_in_minutes=3
-        max_wait_in_seconds=180
-        max_retries=$((max_wait_in_seconds/sleep_time_in_seconds))
-
-        echo "Maximum time to wait in seconds = ${max_wait_in_seconds}"
-        echo "Maximum number of retries = ${max_retries}"
-        
-        count=1
-
-        while ! $sp_exists &> /dev/null
-        do
-
-            echo "Waiting for Service Principal appId to complete provisioning (${count}/${max_retries})"
-            echo "Trying again in ${sleep_time_in_seconds} seconds..."
-            sleep "${sleep_time_in_seconds}"
-
-            if [[ ${count} -eq max_retries ]]; then
-                echo "Provisioning the Service Principal appId has exceeded ${max_wait_in_minutes} minutes. Investigate and re-run script."
-                exit 1
-            fi
-
-            count=$((count +1))
-
-        done
-    fi
+    sp_exists "${mlz_sp_name}" "appId"
 
     sp_clientid=$(az ad sp show \
     --id "http://${mlz_sp_name}" \
@@ -116,38 +124,7 @@ if [[ -z $(az ad sp list --filter "displayName eq '${mlz_sp_name}'" --query "[].
     # Added the sleep below to accomodate for the transient behavior where the Service Principal creation
     # is complete but an immediate query for it will fail. The sleep loop will run for 3 minutes and then
     # the script will exit due to a platform problem
-    sp_exists="az ad sp show \
-        --id http://${mlz_sp_name} \
-        --query objectId"
-
-    if ! $sp_exists &> /dev/null; then
-
-        sleep_time_in_seconds=10
-        max_wait_in_minutes=3
-        max_wait_in_seconds=180
-        max_retries=$((max_wait_in_seconds/sleep_time_in_seconds))
-
-        echo "Maximum time to wait in seconds = ${max_wait_in_seconds}"
-        echo "Maximum number of retries = ${max_retries}"
-        
-        count=1
-
-        while ! $sp_exists &> /dev/null
-        do
-
-            echo "Waiting for Service Principal appId to complete provisioning (${count}/${max_retries})"
-            echo "Trying again in ${sleep_time_in_seconds} seconds..."
-            sleep "${sleep_time_in_seconds}"
-
-            if [[ ${count} -eq max_retries ]]; then
-                echo "Provisioning the Service Principal appId has exceeded ${max_wait_in_minutes} minutes. Investigate and re-run script."
-                exit 1
-            fi
-
-            count=$((count +1))
-
-        done
-    fi
+    sp_exists "${mlz_sp_name}" "objectId"
 
     sp_objid=$(az ad sp show \
         --id "http://${mlz_sp_name}" \
