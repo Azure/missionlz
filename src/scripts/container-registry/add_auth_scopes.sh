19365e06f6db3856d5be6952cfab388fc384ce04
Glenn Musa <4622125+glennmusa@users.noreply.github.com>
2021-04-13 16:07:29 -0400

    wait for sp and sp property creation

--- src/scripts/container-registry/add_auth_scopes.sh
+++ src/scripts/container-registry/add_auth_scopes.sh
@@ -20,52 +20,6 @@ usage() {
   error_log "usage: add_auth_scopes.sh <mlz config file> <mlz UI FQDN url>"
 }
 
-app_exists () {
-
-    app_name=$1
-    app_property=$2
-
-    echo "App name in app check function = ${1}"
-    echo "App ID in app check function = ${2}"
-
-    app_query="az ad app list \
-        --display-name ${app_name} \
-        --query [].${app_property} \
-        --output tsv"
-    
-    sleep 30
-
-    if ! $app_query &> /dev/null; then
-
-        sleep_time_in_seconds=10
-        max_wait_in_minutes=3
-        max_wait_in_seconds=180
-        max_retries=$((max_wait_in_seconds/sleep_time_in_seconds))
-
-        echo "INFO: maximum time to wait in seconds = ${max_wait_in_seconds}"
-        echo "INFO: maximum number of retries = ${max_retries}"
-
-        count=1
-
-        while ! $app_query &> /dev/null
-        do
-
-            echo "INFO: waiting for service principal ${sp_name} to populate property ${sp_property} (${count}/${max_retries})"
-            echo "INFO: trying again in ${sleep_time_in_seconds} seconds..."
-            sleep "${sleep_time_in_seconds}"
-
-            if [[ ${count} -eq max_retries ]]; then
-                error_log "ERROR: unable to determine ${app_property} for the service principal ${app_property} in ${max_wait_in_minutes} minutes. Investigate and re-run script."
-                exit 1
-            fi
-
-            count=$((count +1))
-
-        done
-    fi
-
-}
-
 if [[ "$#" -lt 2 ]]; then
    usage
    exit 1
@@ -82,30 +36,52 @@ fqdn=$2
 required_resources_json_file="$(dirname "$(realpath "${BASH_SOURCE%/*}")")/config/mlz_login_app_resources.json"
 
 # generate app registration
-if [[ -z $(az ad app list --filter "displayName eq '${mlz_fe_app_name}'" --query "[].displayName" -o tsv) ]];then
-    echo "INFO: creating app registration ${mlz_fe_app_name} to facilitate user logon at ${fqdn}..."
-    client_id=$(az ad app create \
-        --display-name "${mlz_fe_app_name}" \
-        --reply-urls "http://${fqdn}/redirect" \
-        --required-resource-accesses "${required_resources_json_file}" \
-        --query appId \
-        --only-show-errors \
-        --output tsv)
-    
-    echo "App ID after app creation = ${client_id}"
+echo "INFO: creating app registration ${mlz_fe_app_name} to facilitate user logon at ${fqdn}..."
+az ad app create \
+  --display-name "${mlz_fe_app_name}" \
+  --reply-urls "http://${fqdn}/redirect" \
+  --required-resource-accesses "${required_resources_json_file}" \
+  --only-show-errors \
+  --output none
 
-    # Get App Registration AppId
-    # Added the sleep below to accomodate for the transient behavior where the Application Registration creation
-    # is complete but an immediate query for it will fail. The sleep loop will run for 3 minutes and then
-    # the script will exit due to a platform problem
-    app_exists "${mlz_fe_app_name}" "appId"
+# wait_for_query_success will attempt the query passed by argument
+# if the query does not return a result within max_wait_in_seconds it will exit.
+wait_for_query_success() {
+  query=$1
 
-    client_password=$(az ad app credential reset \
-        --id ${client_id} \
-        --query password \
-        --only-show-errors \
-        --output tsv)
-fi
+  sleep_time_in_seconds=10
+  max_wait_in_seconds=180
+  max_retries=$((max_wait_in_seconds/sleep_time_in_seconds))
+
+  count=1
+
+  while [[ -z $($query)  ]]
+  do
+      echo "INFO: waiting for query \"${query}\" to return results (${count}/${max_retries})"
+      echo "INFO: trying again in ${sleep_time_in_seconds} seconds..."
+      sleep "${sleep_time_in_seconds}"
+
+      if [[ ${count} -eq max_retries ]]; then
+          error_log "ERROR: unable to get results from query \"${query}\" in ${max_wait_in_seconds} seconds. Investigate and re-run script."
+          exit 1
+      fi
+
+      count=$((count +1))
+  done
+}
+
+# use `wait_for_query_success` to accomodate transient failures where
+# app creation completes but an immediate query for it will fail
+app_id_query="az ad app list --display-name ${mlz_fe_app_name} --query [].appId --output tsv"
+wait_for_query_success "$app_id_query"
+
+client_id=$($app_id_query)
+
+client_password=$(az ad app credential reset \
+    --id ${client_id} \
+    --query password \
+    --only-show-errors \
+    --output tsv)
 
 # update keyvault with the app registration information
 echo "INFO: storing app registration information for client ID ${client_id} in ${mlz_kv_name}..."
