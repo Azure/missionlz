168be710b3a5b0826991062683da82c49e981226
Glenn Musa <4622125+glennmusa@users.noreply.github.com>
2021-07-07 20:03:02 -0400

    call init the same way

--- src/scripts/terraform/apply_terraform.sh
+++ src/scripts/terraform/apply_terraform.sh
@@ -26,52 +26,12 @@ extra_vars=${4:-notset}
 
 scripts_path=$(realpath "${BASH_SOURCE%/*}/..")
 
-# check for dependencies
-. "${scripts_path}/util/checkforazcli.sh"
-. "${scripts_path}/util/checkforterraform.sh"
+# init terraform for the directory
+. "${scripts_path}/terraform/init_terraform.sh" "$tf_dir"
 
-# Validate necessary Azure resources exist
-. "${scripts_path}/config/config_validate.sh" "${tf_dir}"
-
-# Validate configuration file exists
-config_vars="${tf_dir}/config.vars"
-. "${scripts_path}/util/checkforfile.sh" \
-   "${config_vars}" \
-   "The configuration file ${config_vars} is empty or does not exist. You may need to run MLZ setup."
-
-# Source configuration file
-. "${config_vars}"
-
-# Verify Service Principal is valid and set client_id and client_secret environment variables
+# verify Service Principal is valid and set client_id, client_secret, object_id
 . "${scripts_path}/config/get_sp_identity.sh" "${config_vars}"
 
-# Set the terraform state key
-tf_name=$(basename "$(dirname "${tf_vars}")")
-key="${mlz_env_name}${tf_name}"
-
-# declare terraform init arguments for the backend configuration
-init_args=()
-init_args+=("-backend-config=\"metadata_host=${metadata_host}\"")
-init_args+=("-backend-config=\"key=${key}\"")
-init_args+=("-backend-config=\"resource_group_name=${tf_rg_name}\"")
-init_args+=("-backend-config=\"storage_account_name=${tf_sa_name}\"")
-init_args+=("-backend-config=\"container_name=${container_name}\"")
-init_args+=("-backend-config=\"environment=${environment}\"")
-init_args+=("-backend-config=\"tenant_id=${tenant_id}\"")
-init_args+=("-backend-config=\"subscription_id=${sub_id}\"")
-init_args+=("-backend-config=\"client_id=${client_id}\"")
-init_args+=("-backend-config=\"client_secret=${client_secret}\"")
-
-# If there is an offline terraform provider (plugin) cache, append it to the init arguments
-if [[ -d $TF_PLUGIN_CACHE_DIR ]]; then
-   init_args+=("-plugin-dir=${TF_PLUGIN_CACHE_DIR}")
-fi
-
-# Initialize terraform in the configuration directory
-cd "${tf_dir}" || exit
-eval terraform init "${init_args[@]}"
-
-# apply the terraform configuration with global vars and the configuration's tfvars
 apply_command="terraform apply"
 
 if [[ $auto_approve == "y" ]]; then
