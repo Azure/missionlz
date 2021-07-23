104fba684e14545780c72516a3437baa00d38f16
Glenn Musa <4622125+glennmusa@users.noreply.github.com>
2021-07-23 14:41:37 -0400

    check service principal exists

--- src/scripts/util/checkforarmcredential.sh
+++ src/scripts/util/checkforarmcredential.sh
@@ -28,3 +28,11 @@ if [[ -z $ARM_CLIENT_ID || -z $ARM_CLIENT_SECRET ]]; then
   echo "INFO: You can set these environment variables with 'export ARM_CLIENT_ID=\"YOUR_CLIENT_ID\"' and 'export ARM_CLIENT_SECRET=\"YOUR_CLIENT_SECRET\"'"
   exit 1
 fi
+
+sp_exists="az ad sp show --id ${ARM_CLIENT_ID}"
+
+if ! $sp_exists &> /dev/null; then
+  error_log "ERROR: unable to find a Service Principal with Client ID ${ARM_CLIENT_ID}!"
+  echo "INFO: check the value of the environment variable \$ARM_CLIENT_ID and try 'az ad sp show --id \$ARM_CLIENT_ID' to inspect results..."
+  exit 1
+fi
