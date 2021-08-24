7576568dd4c068912560da4914d2a276a61b71a4
shawngib <shawngib@microsoft.com>
2021-08-24 15:27:07 -0400

    fixed formatting of files and readme encoding

--- src/terraform/modules/policy-assignments/main.tf
+++ src/terraform/modules/policy-assignments/main.tf
@@ -8,12 +8,12 @@ resource "azurerm_resource_group_policy_assignment" "policy_assign" {
   name                 = "NIST Assignment - ${data.azurerm_resource_group.rg.name}"
   resource_group_id    = data.azurerm_resource_group.rg.id
   policy_definition_id = var.policy_id
-  location = data.azurerm_resource_group.rg.location
+  location             = data.azurerm_resource_group.rg.location
   identity {
     type = "SystemAssigned"
   }
   # Define parameters for value template file directed to environment
   parameters = templatefile("${path.module}/nist-parameter-values/${var.environment}.json.tmpl", {
-      laws_instance_id = var.laws_instance_id
+    laws_instance_id = var.laws_instance_id
   })
 }
