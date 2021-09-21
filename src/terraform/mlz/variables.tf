bc306495f082457842e87f81bbad0adc7dfe7d83
Lisa Murphy <lisamurphy@microsoft.com>
2021-09-21 11:26:37 -0400

    moving DeploymentName tag to map value

--- src/terraform/mlz/variables.tf
+++ src/terraform/mlz/variables.tf
@@ -11,12 +11,6 @@ variable "tf_environment" {
   default     = "public"
 }
 
-variable "deploymentname" {
-  description = "A name for the deployment. It defaults to dev."
-  type        = string
-  default     = "dev"
-}
-
 variable "mlz_tenantid" {
   description = "The Azure Active Directory tenant ID that should be used for the deployment."
   type        = string
@@ -59,10 +53,13 @@ variable "create_assignment" {
   default     = false
 }
 
-variable "deploymenttype" {
-  description = "Metadata corresponding with the deployment method."
-  type        = string
-  default     = "MissionLandingZoneTF"
+variable "tags" {
+ description = "A map of key value pairs to apply as tags to resources provisioned in this deployment"
+ type        = map(string)
+ default = {
+  "DeploymentName" : "mlz-dev",
+  "DeploymentType" : "MissionLandingZoneTF"
+  }
 }
 #################################
 # Hub Configuration
