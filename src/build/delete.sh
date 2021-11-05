d976997dac76754aeb3c35099ee0e1597db593ed
Vidya Bala <vidbala@microsoft.com>
2021-11-05 19:55:06 -0400

    Update delete.sh

--- src/build/delete.sh
+++ src/build/delete.sh
@@ -6,14 +6,14 @@
 #   Usage: ./delete.sh "<filter to use in RG name search>" "<subscription ID to end in>"
 
 for subscription in `az account list -o tsv`; do
-    az account set --subscription "${subscription"
+    az account set --subscription "${subscription}"
         for rgname in `az group list --query "[? contains(name,'$1')][].{name:name}" -o tsv`; do
         echo Deleting ${rgname}
         az group delete -n ${rgname} --yes --no-wait
         done
         for setting in `az monitor diagnostic-settings subscription list --query "value[? contains(@.name, '$1')].name" -o tsv`; do
             echo Deleting ${setting}
-            az monitor diagnostic-settings delete --name $setting --resource "/subscriptions/${subscription}"
+            az monitor diagnostic-settings delete --name $setting --resource /subscriptions/${subscription}
         done    
 done
 # az account set --subscription $2
