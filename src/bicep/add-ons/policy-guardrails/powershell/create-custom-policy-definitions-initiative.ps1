5045b2d28c2debf12dba546a3027ca1afb444029
brsteel@microsoft.com <brsteel@microsoft.com>
2025-01-28 15:18:41 -0500

    some clean up and mods to create/delete scripts

--- src/bicep/add-ons/policy-guardrails/powershell/create-custom-policy-definitions-initiative.ps1
+++ src/bicep/add-ons/policy-guardrails/powershell/create-custom-policy-definitions-initiative.ps1
@@ -2,17 +2,23 @@
 $emailSecurityContact = "brsteel@microsoft.com"
 $logAnalyticsResourceId = "/subscriptions/6d2cdf2f-3fbe-4679-95ba-4e8b7d9aed24/resourceGroups/bws-rg-operations-network-va-test/providers/Microsoft.OperationalInsights/workspaces/bws-log-operations-va-test"
 $hubVnetResourceId = "/subscriptions/afb59830-1fc9-44c9-bba3-04f657483578/resourceGroups/bws-rg-hub-network-va-test/providers/Microsoft.Network/virtualNetworks/bws-vnet-hub-va-test"
+$folderScope = "Global"
+$vulnerabilityAssessmentsEmail = @("brsteel@microsoft.com")
+$vulnerabilityAssessmentsStorageID = "/subscriptions/6d2cdf2f-3fbe-4679-95ba-4e8b7d9aed24/resourceGroups/bws-rg-operations-network-va-test/providers/Microsoft.Storage/storageAccounts/5bdj25z4mllp6"
+$hubRegion = "usgovvirginia"
+$hubRgName = "bws-rg-hub-network-va-test"
+$ddosName = "bws-ddos-va-test"
 
 # Define the directory containing the guard rail policy definition JSON files
-$policyDefinitionsDirectory = ".\src\bicep\add-ons\policy-guardrails\powershell\test"
+$policyDefinitionsDirectory = ".\src\bicep\add-ons\policy-guardrails\powershell\$($folderScope)"
 
 # Define the management group ID the policy intiative and definitions will be linked to
-$managementGroupId = "operations_and_security"
+$managementGroupId = "DoDIaCManagementGroup"
 
 # Define the policy set definition properties
-$policySetDisplayName = "Test Enterprise Guardrails Initiative"
-$policySetName = "TestEnterpriseGuardrailsInitiative"
-$policySetDescription = "A set of policies to manage the enterprise configurations."
+$policySetDisplayName = "Test $($folderScope) Guardrails Initiative"
+$policySetName = "Test$($folderScope)GuardrailsInitiative"
+$policySetDescription = "A set of policies to manage the $($folderScope) configurations."
 $policySetMetadata = @{
     "category" = "All"
     "version" = "1.0.0"
@@ -44,7 +50,7 @@ foreach ($policyFile in $policyFiles) {
     }
 
     # Add the emailSecurityContact parameter if the policy definition requires it
-    if ($policyJson.name -eq "Deploy-ASC-SecurityContacts") {
+    if ($policyJson.name -eq "Deploy-ASC-SecurityContacts-enterprise") {
         $policyDefinition.properties.parameters.emailSecurityContact = @{
             "type" = "String"
             "defaultvalue" = $emailSecurityContact
@@ -65,10 +71,37 @@ foreach ($policyFile in $policyFiles) {
         }
     }
 
+    if ($policyJson.name -eq "Deploy-Sql-vulnerabilityAssessments") {
+        $policyDefinition.properties.parameters.vulnerabilityAssessmentsEmail = @{
+            "type" = "Array"
+            "defaultvalue" = $vulnerabilityAssessmentsEmail
+        }
+        $policyDefinition.properties.parameters.vulnerabilityAssessmentsStorageID = @{
+            "type" = "String"
+            "defaultvalue" = $vulnerabilityAssessmentsStorageID
+        }
+    }
+
+    if ($policyJson.name -eq "Deploy-DDoSProtection") {
+        $policyDefinition.properties.parameters.rgName = @{
+            "type" = "String"
+            "defaultvalue" = $hubRgName
+        }
+        $policyDefinition.properties.parameters.ddosRegion = @{
+            "type" = "String"
+            "defaultvalue" = $hubRegion
+        }
+        $policyDefinition.properties.parameters.ddosName = @{
+            "type" = "String"
+            "defaultvalue" = $ddosName
+        }
+    }
+
     $policyDefinitionJson = $policyDefinition | ConvertTo-Json -Depth 20
+
     Write-Host "Creating policy definition $($policyJson.name)"
     New-AzPolicyDefinition -Name $policyJson.name -Policy $policyDefinitionJson -ManagementGroupName $managementGroupId > $null
-
+    
     # Add the policy definition to the policy definitions array for connecting into the policy set definition
     $policyDefinitions += @{
         "policyDefinitionId" = "/providers/Microsoft.Management/managementGroups/$managementGroupId/providers/Microsoft.Authorization/policyDefinitions/$($policyJson.name)"
---
+++ src/bicep/add-ons/policy-guardrails/powershell/remove-policy-definitions.ps1
@@ -0,0 +1,19 @@
+# Define the management group ID
+$managementGroupId = "operations_and_security"
+
+# Get all policy definitions for the specified management group
+$policyDefinitions = Get-AzPolicyDefinition -ManagementGroupName $managementGroupId
+
+# Filter out the custom policy definitions
+$customPolicyDefinitions = @()
+foreach ($policyDefinition in $policyDefinitions) {
+    if ($policyDefinition.policyType -eq "Custom") {
+        $customPolicyDefinitions += $policyDefinition
+    }
+}
+
+# Remove each custom policy definition
+foreach ($policyDefinition in $customPolicyDefinitions) {
+    Write-Host "Removing policy definition: $($policyDefinition.Name)"
+    Remove-AzPolicyDefinition -Name $policyDefinition.Name -ManagementGroupName $managementGroupId -Force
+}
