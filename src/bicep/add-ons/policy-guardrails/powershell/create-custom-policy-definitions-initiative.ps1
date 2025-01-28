1122a767e4cf21ace73afc086c5d9f6ae6d8701e
brsteel@microsoft.com <brsteel@microsoft.com>
2025-01-28 15:31:57 -0500

    added comments

--- src/bicep/add-ons/policy-guardrails/powershell/create-custom-policy-definitions-initiative.ps1
+++ src/bicep/add-ons/policy-guardrails/powershell/create-custom-policy-definitions-initiative.ps1
@@ -1,21 +1,32 @@
-#parameter and values
+# before running script, verify the folderScope is where the policy definitions are located in JSON format
+# before running script, verify the managementGroupId is the management group ID the policy initiative and definitions will be linked to
+# before running script, verify the policy parameter values are set properly, if that policy definition is used
+
+#policy definition parameters and values, see lower in script to see the policy definitions they are used in
+#if the policy definition that requires the parameter is not used, the parameter value will not be used, so leave it in the script for future reference with no value
 $emailSecurityContact = "brsteel@microsoft.com"
 $logAnalyticsResourceId = "/subscriptions/6d2cdf2f-3fbe-4679-95ba-4e8b7d9aed24/resourceGroups/bws-rg-operations-network-va-test/providers/Microsoft.OperationalInsights/workspaces/bws-log-operations-va-test"
 $hubVnetResourceId = "/subscriptions/afb59830-1fc9-44c9-bba3-04f657483578/resourceGroups/bws-rg-hub-network-va-test/providers/Microsoft.Network/virtualNetworks/bws-vnet-hub-va-test"
-$folderScope = "Global"
 $vulnerabilityAssessmentsEmail = @("brsteel@microsoft.com")
 $vulnerabilityAssessmentsStorageID = "/subscriptions/6d2cdf2f-3fbe-4679-95ba-4e8b7d9aed24/resourceGroups/bws-rg-operations-network-va-test/providers/Microsoft.Storage/storageAccounts/5bdj25z4mllp6"
 $hubRegion = "usgovvirginia"
 $hubRgName = "bws-rg-hub-network-va-test"
 $ddosName = "bws-ddos-va-test"
 
-# Define the directory containing the guard rail policy definition JSON files
+#script variables
+
+# Define the folder scope where the policy definitions are located, it will define part of the policy initiative name that is created
+$folderScope = "Global"
+
+# Define the directory containing the guard rail policy definition JSON file.   folder structure should map to intiative
 $policyDefinitionsDirectory = ".\src\bicep\add-ons\policy-guardrails\powershell\$($folderScope)"
 
 # Define the management group ID the policy intiative and definitions will be linked to
+# could also be mapped to folder scope name, but not necessary
 $managementGroupId = "DoDIaCManagementGroup"
 
 # Define the policy set definition properties
+# assumes all policy definitions cross all categories, if not, adjust the category value for the intiative to match the policies
 $policySetDisplayName = "Test $($folderScope) Guardrails Initiative"
 $policySetName = "Test$($folderScope)GuardrailsInitiative"
 $policySetDescription = "A set of policies to manage the $($folderScope) configurations."
@@ -24,6 +35,8 @@ $policySetMetadata = @{
     "version" = "1.0.0"
 }
 
+
+# script work
 # Initialize an array to hold the policy definitions
 $policyDefinitions = @()
 
@@ -49,6 +62,8 @@ foreach ($policyFile in $policyFiles) {
         }
     }
 
+    # some specific policy definitions require additional parameters, here is where they are added as "defaultvalue"
+    # possible to do this in other ways, such as a parameters.json file for these specific json policies that is retreived if exist, or a powershell included script separate from this main script
     # Add the emailSecurityContact parameter if the policy definition requires it
     if ($policyJson.name -eq "Deploy-ASC-SecurityContacts-enterprise") {
         $policyDefinition.properties.parameters.emailSecurityContact = @{
@@ -97,8 +112,10 @@ foreach ($policyFile in $policyFiles) {
         }
     }
 
+    #convert powershell hashtable to JSON for the definition to be created
     $policyDefinitionJson = $policyDefinition | ConvertTo-Json -Depth 20
 
+    # Create the policy definition in Azure
     Write-Host "Creating policy definition $($policyJson.name)"
     New-AzPolicyDefinition -Name $policyJson.name -Policy $policyDefinitionJson -ManagementGroupName $managementGroupId > $null
     
@@ -112,7 +129,7 @@ foreach ($policyFile in $policyFiles) {
 $policyDefinitionsJson = $policyDefinitions | ConvertTo-Json -Depth 10
 $policySetMetadataJson = $policySetMetadata | ConvertTo-Json -Depth 10
 
-# Create the policy set definition (policy initiative)
+# Create the policy set definition (policy initiative) with all the policy definitions included
 $policySetDefinition = @{
     "name" = $policySetName
     "displayName" = $policySetDisplayName
