bc478a34a0361e9d5264c7e75f2a74c3bf8aea7c
brsteel@microsoft.com <brsteel@microsoft.com>
2025-01-28 10:37:35 -0500

    categorized policies and added parameter capability

--- src/bicep/add-ons/policy-guardrails/test/create-custom-policy-definitions-initiative.ps1
+++ src/bicep/add-ons/policy-guardrails/test/create-custom-policy-definitions-initiative.ps1
@@ -1,5 +1,10 @@
+#parameter and values
+$emailSecurityContact = "brsteel@microsoft.com"
+$logAnalyticsResourceId = "/subscriptions/6d2cdf2f-3fbe-4679-95ba-4e8b7d9aed24/resourceGroups/bws-rg-operations-network-va-test/providers/Microsoft.OperationalInsights/workspaces/bws-log-operations-va-test"
+$hubVnetResourceId = "/subscriptions/afb59830-1fc9-44c9-bba3-04f657483578/resourceGroups/bws-rg-hub-network-va-test/providers/Microsoft.Network/virtualNetworks/bws-vnet-hub-va-test"
+
 # Define the directory containing the guard rail policy definition JSON files
-$policyDefinitionsDirectory = "C:\Users\brsteel\Documents\repositories\missionlz\src\bicep\add-ons\policy-guardrails\test\policies"
+$policyDefinitionsDirectory = "C:\Users\brsteel\Documents\repositories\missionlz\src\bicep\add-ons\policy-guardrails\test\potential-policy-definitions"
 
 # Define the management group ID the policy intiative and definitions will be linked to
 $managementGroupId = "operations_and_security"
@@ -37,6 +42,30 @@ foreach ($policyFile in $policyFiles) {
             "policyRule" = $policyJson.properties.policyRule
         }
     }
+
+    # Add the emailSecurityContact parameter if the policy definition requires it
+    if ($policyJson.name -eq "Deploy-ASC-SecurityContacts") {
+        $policyDefinition.properties.parameters.emailSecurityContact = @{
+            "type" = "String"
+            "defaultvalue" = $emailSecurityContact
+        }
+    }
+
+    if ($policyJson.name -eq "Deploy-ActivityLogs-to-LA-workspace") {
+        $policyDefinition.properties.parameters.logAnalytics = @{
+            "type" = "String"
+            "defaultvalue" = $logAnalyticsResourceId
+        }
+    }
+
+    if ($policyJson.name -eq "Deny-VNET-Peering-To-Non-Approved-VNETs") {
+        $policyDefinition.properties.parameters.logAnalytics = @{
+            "type" = "String"
+            "defaultvalue" = $hubVnetResourceId
+        }
+    }
+    Deny-VNET-Peering-To-Non-Approved-VNETs
+
     $policyDefinitionJson = $policyDefinition | ConvertTo-Json -Depth 10
     Write-Host "Creating policy definition $($policyJson.name)"
     New-AzPolicyDefinition -Name $policyJson.name -Policy $policyDefinitionJson -ManagementGroupName $managementGroupId > $null
