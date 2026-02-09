b50592449c8b949a774b0c247d6815774430e756
Jason Masten <jamasten@microsoft.com>
2026-02-08 22:54:41 -0500

    Moved files, Added commands to support Entra Cloud Sync install

---
+++ src/add-ons/arcgis-pro/artifacts/New-EntraCloudSyncConfiguration.ps1
@@ -0,0 +1,125 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$CloudSuffix,
+
+    [Parameter(Mandatory = $true)]
+    [string]$DomainName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$TenantId,
+
+    [Parameter(Mandatory = $true)]
+    [string]$UserAssignedManagedIdentityClientId
+)
+
+# Variables
+$ErrorActionPreference = "Stop"
+$MicrosoftGraphEndpoint = "https://graph.microsoft." + $CloudSuffix
+
+# Get an access token for Microsoft Graph using IMDS
+$AccessToken = (Invoke-RestMethod `
+    -Headers @{Metadata="true"} `
+    -Method 'GET' `
+    -Uri $('http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=' + $MicrosoftGraphEndpoint + '&client_id=' + $UserAssignedManagedIdentityClientId)).access_token
+
+# Set headers for Microsoft Graph API calls
+$Headers = @{
+    Authorization = "Bearer $AccessToken"
+    "Content-Type" = "application/json"
+}
+
+# Enable tenant flags for synchronization
+# Required Permissions: Organization.ReadWrite.All
+# https://learn.microsoft.com/graph/api/organization-update?view=graph-rest-beta&tabs=http
+Invoke-RestMethod `
+    -Body '{"onPremisesSyncEnabled":true}' `
+    -Headers $Headers `
+    -Method 'PATCH' `
+    -Uri $($MicrosoftGraphEndpoint + '/beta/organization/' + $TenantId)
+
+# Create the service principal
+# Required Permissions: Application.ReadWrite.All
+# https://learn.microsoft.com/graph/api/applicationtemplate-instantiate?view=graph-rest-beta&tabs=http
+$ServicePrincipalObjectId = (Invoke-RestMethod `
+    -Body $('{"displayName":"' + $DomainName + '"}') `
+    -Headers $Headers `
+    -Method 'POST' `
+    -Uri $($MicrosoftGraphEndpoint + '/beta/applicationTemplates/1a4721b3-e57f-4451-ae87-ef078703ec94/instantiate')).servicePrincipal.objectId
+
+# Update the targeted domain 
+# Required Permissions: Synchronization.ReadWrite.All
+# https://learn.microsoft.com/graph/api/synchronization-serviceprincipal-put-synchronization?view=graph-rest-beta&tabs=http
+# $AppKeyValue = '{\"appKeyScenario\":\"AD2AADPasswordHash\"}'
+# $DomainValue = '{\"domain\":\"' + $DomainName + '\"}'
+# Invoke-RestMethod `
+#     -Body $(@{value = @(@{key = "AppKey"; value = "$AppKeyValue"} , @{key = "Domain"; value = "$DomainValue"})} | ConvertTo-Json -Depth 3 -Compress) `
+#     -Headers $Headers `
+#     -Method 'PUT' `
+#     -Uri $($MicrosoftGraphEndpoint + '/beta/servicePrincipals/' + $ServicePrincipalObjectId + '/synchronization/secrets')
+Invoke-RestMethod `
+    -Body $('{"value":[{"key":"AppKey","value":"{\\\"appKeyScenario\\\":\\\"AD2AADPasswordHash\\\"}"},{"key":"Domain","value":"{\\\"domain\\\":\\\"' + $DomainName + '\\\"}"}]}') `
+    -Headers $Headers `
+    -Method 'PUT' `
+    -Uri $($MicrosoftGraphEndpoint + '/beta/servicePrincipals/' + $ServicePrincipalObjectId + '/synchronization/secrets')
+
+Start-Sleep -Seconds 30
+
+# Enable user and group provisioning
+# Required Permissions: Synchronization.ReadWrite.All
+# https://learn.microsoft.com/graph/api/synchronization-synchronization-post-jobs?view=graph-rest-beta&tabs=http
+$ProvisioningJob = Invoke-RestMethod `
+    -Body '{"templateId":"AD2AADProvisioning"}' `
+    -Headers $Headers `
+    -Method 'POST' `
+    -Uri $($MicrosoftGraphEndpoint + '/beta/servicePrincipals/' + $ServicePrincipalObjectId + '/synchronization/jobs')
+
+# Enable password hash synchronization
+# Required Permissions: Synchronization.ReadWrite.All
+# https://learn.microsoft.com/graph/api/synchronization-synchronization-post-jobs?view=graph-rest-beta&tabs=http
+$PasswordHashJob = Invoke-RestMethod `
+    -Body '{"templateId":"AD2AADPasswordHash"}' `
+    -Headers $Headers `
+    -Method 'POST' `
+    -Uri  $($MicrosoftGraphEndpoint + '/beta/servicePrincipals/' + $ServicePrincipalObjectId + '/synchronization/jobs')
+
+# Get the provisioning job schema
+# Required Permissions: Synchronization.ReadWrite.All
+# https://learn.microsoft.com/graph/api/synchronization-synchronizationschema-get?view=graph-rest-beta&tabs=http
+$ProvisioningJobSchema = Invoke-RestMethod `
+    -Headers $Headers `
+    -Method 'GET' `
+    -Uri $($MicrosoftGraphEndpoint + '/beta/servicePrincipals/' + $ServicePrincipalObjectId + '/synchronization/jobs/' + $ProvisioningJob.id + '/schema')
+
+# Update the provisioning job schema
+# Required Permissions: Synchronization.ReadWrite.All
+# https://learn.microsoft.com/graph/api/synchronization-synchronizationschema-update?view=graph-rest-beta&tabs=http
+Invoke-RestMethod `
+    -Body $($Schema.Replace('ServicePrincipalObjectId',$ServicePrincipalObjectId)).Replace('ProvisioningJobId',$ProvisioningJob.id) `
+    -Headers $Headers `
+    -Method 'PUT' `
+    -Uri $($MicrosoftGraphEndpoint + '/beta/servicePrincipals/' + $ServicePrincipalObjectId + '/synchronization/jobs/' + $ProvisioningJob.id + '/schema')
+
+# Discover the schema directory for the provisioning synchronization job
+# This has always thrown a 500 error and not how to incorpate it. The portal uses this API when deploying a new configuration.
+# Invoke-RestMethod `
+#     -Headers $Headers `
+#     -Method 'POST' `
+#     -Uri $($MicrosoftGraphEndpoint + '/beta/servicePrincipals/' + $ServicePrincipalObjectId + '/synchronization/jobs/' + $ProvisioningJob.id + '/schema/directories/' + $($ProvisioningSchema.directories | Where-Object {$_.name -eq 'Active Directory'}).id + '/discover')
+
+Start-Sleep -Seconds 30
+
+# Start the provisioning synchronization jobs
+# Required Permissions: Synchronization.ReadWrite.All
+# https://learn.microsoft.com/graph/api/synchronization-synchronizationjob-start?view=graph-rest-beta&tabs=http
+Invoke-RestMethod `
+    -Headers $Headers `
+    -Method 'POST' `
+    -Uri $($MicrosoftGraphEndpoint + '/beta/servicePrincipals/' + $ServicePrincipalObjectId + '/synchronization/jobs/' + $ProvisioningJob.id + '/start')
+
+# Start the password hash synchronization jobs
+# Required Permissions: Synchronization.ReadWrite.All
+# https://learn.microsoft.com/graph/api/synchronization-synchronizationjob-start?view=graph-rest-beta&tabs=http
+Invoke-RestMethod `
+    -Headers $Headers `
+    -Method 'POST' `
+    -Uri $($MicrosoftGraphEndpoint + '/beta/servicePrincipals/' + $ServicePrincipalObjectId + '/synchronization/jobs/' + $PasswordHashJob.id + '/start')
---
+++ src/add-ons/arcgis-pro/modules/entra-cloud-sync.bicep
@@ -0,0 +1,97 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT License.
+*/
+
+extension 'br:mcr.microsoft.com/bicep/extensions/microsoftgraph/v1.0:1.0.0'
+
+param accessToken string
+@secure()
+param adminPassword string
+param adminUsername string
+param deploymentNameSuffix string
+param domainName string
+@secure()
+param location string
+param mlzTags object
+param tags object
+param userAssignedManagedIdentityClientId string
+param virtualMachineNames array
+
+// Run command to install Entra Cloud Sync on the domain controllers
+module installEntraCloudSyncAgents '../../../modules/run-command.bicep' = [ for (virtualMachineName, i) in virtualMachineNames: {
+  name: 'install-entra-cloud-sync-${i}-${deploymentNameSuffix}'
+  params: {
+    location: location
+    mlzTags: mlzTags
+    name: 'Install-EntraCloudSyncAgent'
+    parameters: [
+      {
+        name: 'AzureEnvironment'
+        value: environment().name
+      }
+      {
+        name: 'AzureResourceManagerUri'
+        value: environment().resourceManager
+      }
+      {
+        name: 'DomainAdministratorUsername'
+        value: adminUsername
+      }
+      {
+        name: 'DomainName'
+        value: domainName
+      }
+      {
+        name: 'SubscriptionId'
+        value: subscription().subscriptionId
+      }
+      {
+        name: 'TenantId'
+        value: subscription().tenantId
+      }
+      {
+        name: 'UserPrincipalName'
+        value: deployer().userPrincipalName
+      }
+    ]
+    protectedParameters: '[{\'name\':\'AccessToken\',\'value\':\'${accessToken}\'},{\'name\':\'DomainAdministratorPassword\',\'value\':\'${adminPassword}\'}]'
+    script: loadTextContent('../artifacts/Install-EntraCloudSyncAgent.ps1')
+    tags: tags
+    virtualMachineName: virtualMachineName
+  }
+}]
+
+// Run command to provision the Entra Cloud Sync configuration in Entra ID
+module provisionEntraCloudSyncConfiguration '../../../modules/run-command.bicep' = {
+  name: 'provision-entra-cloud-sync-config-${deploymentNameSuffix}'
+  params: {
+    location: location
+    mlzTags: mlzTags
+    name: 'New-EntraCloudSyncConfiguration'
+    parameters: [
+      {
+        name: 'CloudSuffix'
+        value: last(split(split(environment().resourceManager, '/')[2], '.'))
+      }
+      {
+        name: 'DomainName'
+        value: domainName
+      }
+      {
+        name: 'TenantId'
+        value: subscription().tenantId
+      }
+      {
+        name: 'UserAssignedManagedIdentityClientId'
+        value: userAssignedManagedIdentityClientId
+      }
+    ]
+    script: loadTextContent('../artifacts/New-EntraCloudSyncConfiguration.ps1')
+    tags: tags
+    virtualMachineName: virtualMachineNames[0]
+  }
+  dependsOn: [
+    installEntraCloudSyncAgents
+  ]
+}
