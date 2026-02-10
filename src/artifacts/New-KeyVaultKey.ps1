84b97b48f20ba420fe6dded8e54b5e713ba8974d
Jason Masten <jamasten@microsoft.com>
2026-02-09 21:19:03 -0500

    Added scripts

---
+++ src/artifacts/New-KeyVaultKey.ps1
@@ -0,0 +1,58 @@
+Param(
+    [int]$KeyExpirationInDays,
+    [string]$KeyName,
+    [string]$KeyVaultUri,
+    [string]$UserAssignedIdentityClientId
+)
+
+$ErrorActionPreference = 'Stop'
+$WarningPreference = 'SilentlyContinue'
+
+# Get an access token for Azure resources
+$AccessToken = (Invoke-RestMethod `
+        -Headers @{Metadata = "true" } `
+        -Uri $('http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=' + $KeyVaultUri + '&client_id=' + $UserAssignedIdentityClientId)).access_token
+
+# Set header for Azure Management API
+$Headers = @{
+    'Content-Type'  = 'application/json'
+    'Authorization' = 'Bearer ' + $AccessToken
+}
+
+$Body = [PSCustomObject]@{
+    kty            = 'RSA-HSM'
+    key_size       = 4096
+    attributes     = @{
+        enabled = $true
+    }
+    rotationPolicy = @{
+        attributes      = @{
+            expiryTime = $('P' + $KeyExpirationInDays + 'D')
+        }
+        lifetimeActions = @(
+            @{
+                action = @{
+                    type = 'Notify'
+                }
+                trigger = @{
+                    timeBeforeExpiry = 'P10D'
+                }
+            },
+            @{
+                action = @{
+                    type = 'Rotate'
+                }
+                trigger = @{
+                    timeAfterCreate = $('P' + ($KeyExpirationInDays - 7) + 'D')
+                }
+            }
+        )
+    }
+}
+
+# Create key vault key
+Invoke-RestMethod `
+    -Body ($Body | ConvertTo-Json -Depth 4) `
+    -Headers $Headers `
+    -Method 'POST' `
+    -Uri $($KeyVaultUri + '/keys/' + $KeyName + '?api-version=2023-09-05') | Out-Null
